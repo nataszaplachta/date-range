4f6a9c9d576df673cbc23de75959819e280065d3
Natasza Plachta <natasza.plachta@gmail.com>
2019-06-26 10:35:22 +0200

    starting the project

---
+++ Dates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Daty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write Your dates:");
+            string input = Console.ReadLine();
+
+                string[] split = input.Split('.', ' ');
+                bool isNumeric = split.All(s => s.All(Char.IsDigit));
+                if (isNumeric == false)
+                {
+                    Console.WriteLine("Dates must be numeric");
+
+                }
+               else
+            { 
+                if (split[1] == split[4] && split[2] == split[5])
+                {
+                    Console.WriteLine($"{split[0]} - {split[3]}.{split[4]}.{split[5]}");
+                }
+                if (split[2] == split[5])
+                {
+                    Console.WriteLine($"{split[0]}.{split[1]} - {split[3]}.{split[4]}.{split[5]}");
+                }
+                else
+                {
+                    Console.WriteLine($"{split[0]}.{split[1]}.{split[2]} - {split[3]}.{split[4]}.{split[5]}");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
